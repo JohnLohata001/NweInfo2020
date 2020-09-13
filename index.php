7bdcfda55b09b1ac4e1adb5a1ac87262eb41ba9f
JohnLohata001 <37670263+JohnLohata001@users.noreply.github.com>
2020-09-12 20:37:15 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/navbar.php"); ?>
+<?php include("includes/silder.php"); ?>
+
+<section class="section text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="section-title">
+                    <span class="text-red">Get free lessons in web</span>
+                    <span class="text-blue">at Tech Deve Info</span> </h2>
+                <div class="underline1 mr-auto ml-auto mb-3"></div>
+                <p class="section-subtitle">
+                    <b> We are doing a good job in Tech Deve Info as IT</b>
+                </p>
+            </div>
+            <div class="col-md-12">
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section bg-f2f2f2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="section-title text-center"> <span class="text-danger">Our</span> Service</h2>
+                <div class="underline mr-auto ml-auto mb-3 red"></div>
+                <p class="section-subtitle text-center">
+                    We are doing a god job in Tech Deve Info as IT
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <img src="images/services/serv1.jpg" alt="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Residential</h5>
+                        <div class="underline mr-auto ml-auto mb-3 red"></div>
+                        <p class="card-text">
+                            some quick example to build the card the
+                        </p>
+                        <a href="#" class="readmore">read more</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <img src="images/services/serv2.jpg" alt="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Residential</h5>
+                        <div class="underline mr-auto ml-auto mb-3 red"></div>
+                        <p class="card-text">
+                            some quick example to build the card the
+                        </p>
+                        <a href="#" class="readmore">read more</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <img src="images/services/serv3.jpg" alt="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Residential</h5>
+                        <div class="underline mr-auto ml-auto mb-3 red"></div>
+                        <p class="card-text">
+                            some quick example to build the card the
+                        </p>
+                        <a href="#" class="readmore">read more</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <img src="images/services/serv4.jpg" alt="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Residential</h5>
+                        <div class="underline mr-auto ml-auto mb-3 red"></div>
+                        <p class="card-text">
+                            some quick example to build the card the
+                        </p>
+                        <a href="#" class="readmore">read more</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section bg-howitworks">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="section-title text-white">
+                    <span class="text-red">How</span>
+                    It Works
+                </h2>
+                <div class="underline mr-auto ml-auto mb-3 red"></div>
+                <p class="section-subtitle text-white">The best service provide by tech deve info web IT</p>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Meet customer </h5>
+                        <div class="underline red mb-3"></div>
+                        <p class="card-text">
+                            somr uick example text to build the title of
+                            the card and make up quick card content
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Understand the need</h5>
+                        <div class="underline red mb-3"></div>
+                        <p class="card-text">
+                            somr uick example text to build the title of
+                            the card and make up quick card content
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Execution </h5>
+                        <div class="underline red mb-3"></div>
+                        <p class="card-text">
+                            somr uick example text to build the title of
+                            the card and make up quick card content
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<section class="section bg-f2f2f2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="section-title text-center"> <span class="text-danger">Clients</span> Testimonials</h2>
+                <div class="underline mr-auto ml-auto mb-3 red"></div>
+                <p class="section-subtitle text-center">
+                    We are doing a god job in Tech Deve Info as IT
+                </p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body text-center">
+
+                        <div class="owl-carousel testimonial owl-theme">
+                            <div class="item">
+                                <div class="test-image">
+                                    <img src="images/user_male2-512.png" alt="image" class="">
+                                </div>
+                                <div class="test-content">
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nisi optio quam modi recusandae
+                                        natus? Incidunt harum reiciendis error voluptates saepe asperiores quasi, natus architecto ipsa blanditiis
+                                        corrupti. Cumque quia sed architecto, rem consequuntur, veritatis cupiditate autem, esse dolorem pariatur at.
+                                        Fugiat odio inventore odit molestias qui voluptate totam quisquam.
+                                    </p>
+                                    <h5>Client 1</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include("includes/scripts.php"); ?>
+<script>
+    $('.testimonial').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+</script>
+<?php include("includes/footer.php"); ?>
